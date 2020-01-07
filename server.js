9f404fafd0f51e157b6a87030eb4810ade21e193
nikhil@125 <nikhil051097@gmail.com>
2020-01-08 05:12:45 +0530

    fixed server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+//Core Library Imports
+const http = require('http');
+
+//External Library Imports
+const cors =require('cors');
+const express = require('express');
+const bodyParser=require('body-parser');
+const morgan = require('morgan');
+
+// Inject dotenv environment 
+require('dotenv').config();
+
+//Router File Import
+const router=require('./router');
+
+//Initializing Express App
+var app = express();
+
+
+//Create Server using http
+var server = http.createServer(app);
+server.timeout = 30000;
+
+//pass a http.Server instance to socket.io
+var io = require('socket.io').listen(server);
+
+//Registering Middleware
+app.use(bodyParser.json({type:'*/*',limit: '50mb',parameterLimit: 50000 }));
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
+app.use(cors({origin:"*"}))
+
+router(app)
+
+const port = process.env.PORT || 8080
+
+server.listen(port,()=>{
+	console.log(`\x1b[1mServer Started at Port: ${port}`)
+})
+
