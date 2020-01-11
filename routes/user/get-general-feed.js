2aa32bf1ab01ffd6660757945bc21caf8b296860
nikhil@125 <nikhil051097@gmail.com>
2020-01-11 21:35:17 +0530

    improved get-category code response structure

--- routes/user/get-general-feed.js
+++ routes/user/get-general-feed.js
@@ -1,34 +1,24 @@
-// const { getGeneralFeed } = require('../../controllers/helpers/elasticSearch-helpers/get-general-feed')
 const { esClient } = require('./../../conf/elastic-conf');
 
 const getGeneralFeed = (req, res) => {
+
     return esClient.get({
         index: 'feed',
         id: 'general-feed'
-    }).then(postIds => {
-
-            // console.log("sould be an array", postIds._source)
-
-            esClient.mget({
-                index: 'post',
-                body:{
-                    ids:postIds._source.postIds
-
-                }
-                
-
-            }).then(posts => {
-                console.log("fetched posts using mget" )
-                return res.status(200).json({posts})
-            }).catch(e=>{
-                console.log("error",e);
-                res.status(400).end();
-            })
-
+    }).then((postIds) => {
+        esClient.mget({
+            index: 'post',
+            body: { ids: postIds._source.postIds }
+        }).then((data) => {
+            let posts = data.docs.map(post => { return { postId: post._id, ...post._source } })
+            return res.status(200).json({ posts })
         }).catch(e => {
-            console.log("error", e)
             res.status(400).end();
-        })
+        });
+    }).catch(e => {
+        console.log("error", e)
+        res.status(400).end();
+    });
 
 
 }
