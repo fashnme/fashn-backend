03414c48cec7e8ba1401e20b28d469fded76875b
savezsiddiqui <savezsiddiqui@gmail.com>
2020-01-14 18:04:19 +0530

    added delete post and delete story

--- routes/user/delete-post.js
+++ routes/user/delete-post.js
@@ -2,12 +2,22 @@ const { esClient } = require('../../conf/elastic-conf');
 
 const deletePost = (req, res) => {
 
-    // esClient.delete({
-    //     index: "post",
-    //     id: req.body.postId
-    // })
-    //     .then((data) => res.status(200).end())
-    //     .catch(error => console.log("error in deleting post", error))
+    esClient.get({
+        index: 'post',
+        id: req.body.postId
+    })
+        .then(entry => {
+            console.log(entry)
+            if (entry._source.userId === req._id) {
+                esClient.delete({
+                    index: 'post',
+                    id: req.body.postId
+                })
+                    .then(data => res.status(200).end())
+                    .catch(err => { console.log(err); res.status(500).end() })
+            }
+        })
+        .catch(error => console.log("error in deleting post", error))
 }
 
 module.exports = {
