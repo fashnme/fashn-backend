386a3d1d6b75e502537f6c8d5e587f529dd08459
savezsiddiqui <savezsiddiqui@gmail.com>
2020-01-17 15:01:55 +0530

    removed extra code from get-user-profile

--- routes/user/get-user-profile.js
+++ routes/user/get-user-profile.js
@@ -6,29 +6,29 @@ const getUserProfile = (req, res) => {
     console.log(req._id)
 
     esClient.get({
-    index:'user',  
-    id:req._id
-    }).then(data=>{
+        index: 'user',
+        id: req._id
+    }).then(data => {
 
         //   using this approach - can be optimised for cleaner code
-        let coreProfileData={}
-        
-        coreProfileData.firstName=data._source.firstName
-        coreProfileData.lastName=data._source.lastName
-        coreProfileData.gender=data._source.gender
-        coreProfileData.userName=data._source.userName
-        coreProfileData.profilePic=data._source.profilePic
-        
+        // let coreProfileData={}
 
-        res.json({user: coreProfileData})
-        
-    }).catch(e=>{
+        // coreProfileData.firstName=data._source.firstName
+        // coreProfileData.lastName=data._source.lastName
+        // coreProfileData.gender=data._source.gender
+        // coreProfileData.userName=data._source.userName
+        // coreProfileData.profilePic=data._source.profilePic
+
+
+        res.json({ user: data._source })
+
+    }).catch(e => {
         console.log("error in fetching user", e)
         res.status(404).end()
     })
 
 
 }
-module.exports={
+module.exports = {
     getUserProfile
 }
