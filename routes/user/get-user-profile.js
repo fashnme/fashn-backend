7a8027067bdcff120c4d0df5a1c079887ad1091d
nikhil@125 <nikhil051097@gmail.com>
2020-01-27 19:17:52 +0530

    fixed timestamp typo in create post and get user profile

--- routes/user/get-user-profile.js
+++ routes/user/get-user-profile.js
@@ -3,26 +3,57 @@ const { esClient } = require('./../../conf/elastic-conf');
 
 const getUserProfile = (req, res) => {
 
-        esClient.get({
-            index: 'user',
-            id: req.body.userId
-        }).then(data => {
-            
-            if(req.body.userId ==  req._id){
-                return res.json({ user: data._source });
-            }else{
-                let { firstName, lastName, gender, profilePic, topPosts, totalLikes, followersCount, followingCount, userName} = data._source;
-                return res.json({user:{firstName, lastName, gender,profilePic, topPosts, totalLikes, followersCount, followingCount, userName}})
+    let body = {
+        sort: [{ timestamp: { "order": "desc" } }],
+        query: {
+            match: {
+                userId: req.body.userId
             }
-    
-        }).catch(e => {
-            console.log("error in fetching user", e)
-            res.status(404).end()
-        })
+        }
+    }
 
-   
-    
-    
+    esClient.get({
+        index: 'user',
+        _source: ['firstName', 'lastName', 'gender', 'profilePic', 'totalLikes', 'followersCount', 'followingCount', 'userName'],
+        id: req.body.userId
+    }).then((data) => {
+
+        let userDetails = data._source;
+
+        esClient.search({
+            index: ['post'],
+            size: 18,
+            body
+        }).then((data) => {
+            
+            let recentPosts = data.hits.hits.map(ele => ele._id);
+
+            esClient.search({
+                index: ['like'],
+                size: 18,
+                body
+            }).then(data => {
+                
+                let recentLikedPosts = data.hits.hits.map(ele => ele._id);
+
+                return res.status(200).json({
+                    user: { ...userDetails, recentPosts, recentLikedPosts }
+                })
+            })
+            .catch(e => {
+                return res.status(500).send(`Server Error`);
+            });
+        })
+        .catch(e=>{
+            return res.status(500).send(`Server Error`);
+        })
+    }).catch(e=>{
+        return res.status(500).send(`Server Error`);
+    });
+
+
+
+
 
 
 }
