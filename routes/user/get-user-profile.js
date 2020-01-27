b6a9a1457a8ec883853f3c126a9727f54152f717
nikhil@125 <nikhil051097@gmail.com>
2020-01-27 16:40:11 +0530

    generalized get-user-profile

--- routes/user/get-user-profile.js
+++ routes/user/get-user-profile.js
@@ -3,19 +3,26 @@ const { esClient } = require('./../../conf/elastic-conf');
 
 const getUserProfile = (req, res) => {
 
-    console.log(req._id)
+        esClient.get({
+            index: 'user',
+            id: req.body.userId
+        }).then(data => {
+            
+            if(req.body.userId ==  req._id){
+                return res.json({ user: data._source });
+            }else{
+                let { firstName, lastName, gender, profilePic, topPosts, totalLikes, followersCount, followingCount, userName} = data._source;
+                return res.json({user:{firstName, lastName, gender,profilePic, topPosts, totalLikes, followersCount, followingCount, userName}})
+            }
+    
+        }).catch(e => {
+            console.log("error in fetching user", e)
+            res.status(404).end()
+        })
 
-    esClient.get({
-        index: 'user',
-        id: req._id
-    }).then(data => {
-
-        res.json({ user: data._source })
-
-    }).catch(e => {
-        console.log("error in fetching user", e)
-        res.status(404).end()
-    })
+   
+    
+    
 
 
 }
